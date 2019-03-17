648a3c870026808459a28b7c0f4ca95e7b0db480
kaushlendra277 <kaushlendra277@gmail.com>
2019-03-16 22:29:01 -0700

    added git ignore file and a test project

---
+++ Java-TrickyProblem/Java-Tricky/src/root/SOE.java
@@ -0,0 +1,12 @@
+package root;
+
+public class SOE {
+
+	SOE(){
+		System.out.println("in constructor");
+	}
+	SOE obj = new SOE();
+	public static void main(String[] args) {
+		SOE obj2 = new SOE();
+	}
+}
